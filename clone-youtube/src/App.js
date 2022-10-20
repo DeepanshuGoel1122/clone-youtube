5654d73c75b41c1b9b6a4aecf4257726f1a3bce0
Mansi Raj <99664468+mansi0812@users.noreply.github.com>
2022-10-20 11:21:34 +0530

    Updated App.js, added all components routes

--- clone-youtube/src/App.js
+++ clone-youtube/src/App.js
@@ -1,10 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { Box } from "@mui/material";
 
-function App() {
-  return (
-    <div>
-      hello world
-    </div>
-  );
-}
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed } from './components';
+
+const App = () => (
+    <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+        <Navbar/>
+        <Routes>
+            <Route path="/" exact element={<Feed />} />
+            <Route path="/video/:id" element={<VideoDetail />} />
+            <Route path="/channel/:id" element={<ChannelDetail />} />
+            <Route path="/search/:searchterm" element={<SearchFeed />} />
+        </Routes>
+    </Box>
+
+    </BrowserRouter>
+);
 
 export default App;
