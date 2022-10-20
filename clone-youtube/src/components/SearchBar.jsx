1acc8b7459cc97f71976171a93bffd3b0ce4261f
Mansi Raj <99664468+mansi0812@users.noreply.github.com>
2022-10-20 11:48:07 +0530

    Added searchfeed and searchbar

---
+++ clone-youtube/src/components/SearchBar.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const SearchBar = () => {
+  return (
+    <Paper
+      component='form'
+      onSubmit={() => {}}
+      sx={{
+        borderRadius: 20,
+        border: '1px solid #e3e3e3',
+        pl: 2,
+        boxShadow: 'none',
+        mr: { sm: 5 },
+      }}
+    >
+    </Paper>
+  );
+};
+
+export default SearchBar
