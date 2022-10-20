56ea0365c5d466f60c25d8a7d72c625ed15834ce
dpdp11 <82877758+dpdp11@users.noreply.github.com>
2022-10-20 11:51:07 +0530

    fix in searchbar, make it working

--- clone-youtube/src/components/SearchBar.jsx
+++ clone-youtube/src/components/SearchBar.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Paper, IconButton} from "@mui/material";
+import { Search } from "@mui/icons-material";
 
 
 const SearchBar = () => {
@@ -15,6 +17,15 @@ const SearchBar = () => {
         mr: { sm: 5 },
       }}
     >
+      <input
+        className='search-bar'
+        placeholder='Search...'
+        value=""
+        onChange={() => {}}
+      />
+      <IconButton type='submit' sx={{ p: '10px', color: 'red' }} aria-label='search'>
+        <Search />
+      </IconButton>
     </Paper>
   );
 };
